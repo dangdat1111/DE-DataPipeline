759cbf23243ead8f5283579883cac3771a87fdaa
dangdat1111 <dangdat234678@gmail.com>
2025-10-13 22:27:33 +0700

    execute Mysql client + Mongdb client

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from databases.msql_connect import MySQlConnect
+from databases.mongodb_connect import MongoDBConnect
 from config.database_config import get_database_config
 def main(config):
-    with MySQlConnect(config["mysql"].host, config["mysql"].port, config["mysql"].user, config["mysql"].password) as mysql_client:
+    # mysql
+    config_mysql = config["mysql"]
+    with MySQlConnect(config_mysql.host, config_mysql.port, config_mysql.user, config_mysql.password) as mysql_client:
         mysql_client.connect()
 
+
+    #mongodb
+    config_mongodb = config["mongodb"]
+    with MongoDBConnect(config_mongodb.uri, config_mongodb.database) as mongo_client:
+        mongo_client.connect()
 
 if __name__ == "__main__":
     config = get_database_config()
