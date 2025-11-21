f4522b0822af658cce8722cc6411ef62f2a51654
Tesseract-OptimusPrime <dangdat234678@gmail.com>
2025-11-21 23:10:25 +0700

    fix : create mysql schema

--- databases/schema_manager.py
+++ databases/schema_manager.py
@@ -9,22 +9,33 @@ def create_mysql_schema(connection, cursor):
     print(f"---------_DROP database : {database} in MYSQL---------")
     cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
     print(f"---------_CREATE database : {database} in MYSQL---------")
-
-    connection.database = database
     connection.commit()
+    connection.database = database
+
 
     try:
         with open('/home/prime/dangdat1111/DE-DataPipeline/src/sql/schema.sql', 'r') as f:
             sql_script = f.read()
-            # print(sql_script)
-            # cursor.execute(f"USE {database}")
-            cursor.execute(sql_script)
+            # thực thi từng câu lệnh trong file schema.sql
+            sql_commands = [cmd.strip() for cmd in sql_script.split(";") if cmd.strip()]
+            for cmd in sql_commands:
+                cursor.execute(cmd)
+                print(f'--------------Executed Mysql Command: {cmd}-----------')
+
+            # thực thi toàn bộ file schema.sql
+            # cursor.execute(sql_script)
             connection.commit()
             print("----------_CREATED MYSQL SCHEMA-------------")
     except Error as e:
         connection.rollback()
         raise Exception(f"-----------Failed to CREATE MYSQL SCHEMA. ERROR : {e}") from e
 
-# def create_mongodb_schema()
+def create_mongodb_schema(db):
+    db.drop_collection("users")
+    db.drop_collection("orgs")
+    db.drop_collection("repos")
+    db.drop_collection("events")
+
+
 
 
